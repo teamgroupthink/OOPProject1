d31d805271dd2e47b3c9c9c3e2d99168365b087e
James Montis <jmontis@gmail.com>
2021-04-14 11:39:50 -0500

    Add files via upload

---
+++ HazMatMain.java
@@ -0,0 +1,23 @@
+/*
+
+  ==============================
+	HazMatMain.java - OOP Team 2 
+	* 
+	* Patrick Carroll, Aaron Fortner, James Montis
+  ==============================
+  * 
+  
+  
+*/
+
+public class HazMatMain {
+
+		public static void main(String args[]){
+			
+			System.out.println("hey");
+			
+			
+		}
+	
+	
+}
